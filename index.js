11b0efadfb9972131dcddff569eb024dd8d9ccb9
lolodev <horaciodev1706@gmail.com>
2025-07-20 19:30:00 -0300

    War - Callbacks - revisiting array.filter

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 /**
  Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
- 
+
  URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
- 
+
  Log the whole response to the console
  */
 
-const handleClick = () => {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => console.log(data));
-};
+// const handleClick = () => {
+//     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+//         .then(res => res.json())
+//         .then(data => console.log(data));
+// };
 
-document.querySelector("#new-deck").addEventListener("click", handleClick);
+// document.querySelector("#new-deck").addEventListener("click", handleClick);
 
-const twoSecondsLater = () => {
-    console.log("I finally ran!");
-};
+// const twoSecondsLater = () => {
+//     console.log("I finally ran!");
+// };
 
-setTimeout(twoSecondsLater, 2000);
+// setTimeout(twoSecondsLater, 2000);
+
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+];
+
+const hasPets = person => person.hasPet;
+
+
+const peopleWithPets = people.filter(hasPets);
+console.log(peopleWithPets);
