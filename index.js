5f3644c16d0074d646934db1dceb55112afce77f
lolodev <horaciodev1706@gmail.com>
2025-07-20 22:26:57 -0300

    War - Callbacks - make own filterArray function

--- index.js
+++ index.js
@@ -21,14 +21,26 @@
 // setTimeout(twoSecondsLater, 2000);
 
 const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
+    { name: "Jack", hasPet: true, age: 12 },
+    { name: "Jill", hasPet: false, age: 18 },
+    { name: "Alice", hasPet: true, age: 22 },
+    { name: "Bob", hasPet: false, age: 32 },
 ];
 
-const hasPets = person => person.hasPet;
+// const hasPets = person => person.hasPet;
 
+// const peopleWithPets = people.filter(hasPets);
+// console.log(peopleWithPets);
 
-const peopleWithPets = people.filter(hasPets);
-console.log(peopleWithPets);
+// const peopleOver18 = people.filter(person => person.age >= 18);
+// console.log(peopleOver18);
+
+function filterArray(array, callback) {
+    const resultingArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            resultingArray.push(array[i]);
+        }
+    }
+    return resultingArray;
+}
