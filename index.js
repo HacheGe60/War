8550e69a412c062073f52019b6cf2e83ede344e6
lolodev <horaciodev1706@gmail.com>
2025-07-24 19:13:28 -0300

    War- draw to cards from our deck

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 let deckId;
+const drawCardEl = document.querySelector("#draw-card");
+const newDeckEl = document.querySelector("#new-deck");
 
-const handleClick = () => {
+drawCardEl.disabled = true;
+newDeckEl.disabled = false;
+
+function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id;
         });
-};
+    drawCardEl.disabled = false;
+    newDeckEl.disabled = true;
+}
 
-document.querySelector("#new-deck").addEventListener("click", handleClick);
+function drawCard() {
+    if (!deckId) {
+        alert("Please select a new deck first!");
+        return;
+    }
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
+}
+
+newDeckEl.addEventListener("click", handleClick);
+drawCardEl.addEventListener("click", drawCard);
+
+
